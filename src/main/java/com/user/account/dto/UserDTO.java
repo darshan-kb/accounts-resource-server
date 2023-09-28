9a22cbdad3e4ca95e08157a3389e685b538ff246
darshan-kb <darshan.kb@outlook.com>
2023-09-28 21:27:08 +0530

    account creation added in game server

--- src/main/java/com/user/account/dto/UserDTO.java
+++ src/main/java/com/user/account/dto/UserDTO.java
@@ -2,11 +2,13 @@ package com.user.account.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private String username;
     private String email;
--- src/main/java/com/user/account/service/UserService.java
+++ src/main/java/com/user/account/service/UserService.java
@@ -4,13 +4,27 @@ import com.user.account.dto.UserDTO;
 import com.user.account.entities.User;
 import com.user.account.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private TokenService tokenService;
     public String createUser(UserDTO user){
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.set("Authorization","Bearer "+tokenService.getToken());
+        HttpEntity<String> entity = new HttpEntity(user ,headers2);
+
+        ResponseEntity<UserDTO> response2 = restTemplate.exchange("http://localhost:9090/account", HttpMethod.POST, entity, UserDTO.class);
+        System.out.println(response2.getBody());
         return userRepository.save(new User(user)).toString();
     }
 
