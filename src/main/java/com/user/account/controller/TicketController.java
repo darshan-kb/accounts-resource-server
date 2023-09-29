ee1b9cdfeb3caac74be3d550bd7c2c549f047881
darshan-kb <darshan.kb@outlook.com>
2023-09-30 01:11:34 +0530

    account creation added in game server

--- src/main/java/com/user/account/controller/TicketController.java
+++ src/main/java/com/user/account/controller/TicketController.java
@@ -1,6 +1,7 @@
 package com.user.account.controller;
 
 import com.user.account.model.ApiResponse;
+import com.user.account.payload.AddTicketPayload;
 import com.user.account.service.TicketService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,8 @@ public class TicketController {
     @Autowired
     TicketService ticketService;
     @PostMapping("/ticket/add")
-    public ResponseEntity<Double> addticket(@RequestBody String email, @RequestBody double amount){
-        double balance = ticketService.addTicket(email,amount);
+    public ResponseEntity<Double> addticket(@RequestBody AddTicketPayload addTicketPayload){
+        double balance = ticketService.addTicket(addTicketPayload.getEmail(), addTicketPayload.getAmount());
         return new ResponseEntity<Double>(balance,HttpStatus.OK);
     }
 }
